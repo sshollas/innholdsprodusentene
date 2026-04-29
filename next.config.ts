36cc8b446d7384825a1883a302913c5d0a394249
sshollas <sshollas@gmail.com>
2026-04-29 16:06:12 +0200

    next-mxdv6

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   pageExtensions: ['ts', 'tsx', 'mdx'],
+  serverExternalPackages: ['gray-matter'],
 }
 
 export default nextConfig
